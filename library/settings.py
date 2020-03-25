c7f46d80fa5c5ed7250851557476c63dd1ccfda0
glech <glech@glechpc>
2020-03-25 20:26:31 +0300

    added pagination

--- library/settings.py
+++ library/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 CORS_ORIGIN_ALLOW_ALL = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
 
 # Application definition
 
