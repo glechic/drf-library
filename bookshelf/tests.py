5faefa1497401b3dfa997da6469551b72ce11edc
glech <glech@glechpc>
2020-03-26 16:31:05 +0300

    added tests for user view

--- bookshelf/tests.py
+++ bookshelf/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from .models import Book, User
+
+
+class UserTestCase(APITestCase):
+    url = reverse('bookshelf:users')
+    fixtures = ['users.json']
+
+    def test_list_users(self):
+        self.response = self.client.get(self.url)
+        self.assertEqual(200, self.response.status_code)
+        self.content = json.loads(self.response.content)
+        self.assertEqual(User.objects.count(), len(self.content))
+
+    def test_create_user(self):
+        self.before_content = json.loads(self.client.get(self.url).content)
+        self.response = self.client.post(self.url, {'name': 'Kirill Hulin'})
+        self.single_content = json.loads(self.response.content)
+        self.assertEqual(201, self.response.status_code)
+        self.after_content = json.loads(self.client.get(self.url).content)
+        self.assertEqual(self.after_content, [
+            *self.before_content,
+            self.single_content,
+        ])
